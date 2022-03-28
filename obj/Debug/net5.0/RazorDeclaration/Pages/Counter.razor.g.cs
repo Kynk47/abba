7d2e332e27a1bbdbf0d8a15bbd94771a95aabf4f
antonyking047@gmail.com <antonyking047@gmail.com>
2022-03-28 09:45:41 +0200

    s

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace nonloso.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 1 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 2 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 3 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 4 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 5 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 6 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 7 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 8 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 9 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using nonloso;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\PC11\Source\Repos\Kynk47\abba\_Imports.razor"
+#line 10 "C:\Users\PC11\source\repos\Kynk47\abba\_Imports.razor"
 using nonloso.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using nonloso.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\PC11\Source\Repos\Kynk47\abba\Pages\Counter.razor"
+#line 9 "C:\Users\PC11\source\repos\Kynk47\abba\Pages\Counter.razor"
        
     private int currentCount = 0;
 
